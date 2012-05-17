74571eb57eef722cf31873f8b2f874256088c89c
Akos Polster <akos@pipacs.com>
2012-05-17 02:55:04 +0200

    Add back o2facebook.cpp.

---
+++ o2facebook.cpp
@@ -0,0 +1,46 @@
+#include "o2facebook.h"
+
+static const char *FbEndpoint = "https://graph.facebook.com/oauth/authorize";
+static const char *FbTokenUrl = "https://graph.facebook.com/oauth/access_token";
+static const char *FbRefreshTokenUrl = "https://graph.facebook.com/oauth/access_token"; // FIXME
+static const quint16 FbLocalPort = 1965;
+
+O2Facebook::O2Facebook(const QSting &clientId, const QString &clientSecret, const QString &scope, QObject *parent):
+    O2(clientId, clientSecret, scope, QUrl(FbEndpoint), QUrl(FbTokenUrl), QUrl(FbRefreshTokenUrl), FbLocalPort, parent) {
+}
+
+void O2Facebook::onVerificationReceived(const QMap<QString, QString> response) {
+    qDebug() << "O2Facebook::onVerificationReceived";
+    emit closeBrowser();
+    if (response.contains("error")) {
+        qDebug() << " Verification failed";
+        emit linkingFailed();
+        return;
+    }
+
+    // Save access code
+    setCode(response.value(QString("code")));
+
+    // Exchange access code for access/refresh tokens
+    QUrl url(tokenUrl_);
+    url.addQueryItem("client_id", clientId_);
+    url.addQueryItem("redirect_uri", redirectUri_);
+    url.addQueryItem("scope", scope_);
+    url.addQueryItem("client_secret", clientSecret_);
+    url.addQueryItem("code", code());
+
+    QNetworkRequest tokenRequest(tokenUrl_);
+    tokenRequest.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+    QMap<QString, QString> parameters;
+    parameters.insert("code", code());
+    parameters.insert("client_id", clientId_);
+    parameters.insert("client_secret", clientSecret_);
+    parameters.insert("redirect_uri", redirectUri_);
+    parameters.insert("grant_type", "authorization_code");
+    QByteArray data = buildRequestBody(parameters);
+    qDebug() << " Token request URL:" << url.toString();
+    QNetworkReply *tokenReply = manager_->get(tokenRequest);
+    timedReplies_.add(tokenReply);
+    connect(tokenReply, SIGNAL(finished()), this, SLOT(onTokenReplyFinished()), Qt::QueuedConnection);
+    connect(tokenReply, SIGNAL(error(QNetworkReply::NetworkError)), this, SLOT(onTokenReplyError(QNetworkReply::NetworkError)), Qt::QueuedConnection);
+}
