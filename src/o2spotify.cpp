1928b3d8f795e081dcd4014b4bce2d1706964cb2
Lukas Vogel <lukedirtwalker@gmail.com>
2016-10-23 16:02:58 +0200

    Add spotify oauth classes

---
+++ src/o2spotify.cpp
@@ -0,0 +1,9 @@
+#include "o2spotify.h"
+
+static const char *SpotifyEndpoint = "https://accounts.spotify.com/authorize";
+static const char *SpotifyTokenUrl = "https://accounts.spotify.com/api/token";
+
+O2Spotify::O2Spotify(QObject *parent): O2(parent) {
+    setRequestUrl(SpotifyEndpoint);
+    setTokenUrl(SpotifyTokenUrl);
+}
