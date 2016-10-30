6975dad7d335eeec8b71730af7914836a006437d
Lukas Vogel <lukedirtwalker@gmail.com>
2016-10-30 18:18:02 +0100

    Set spotify refresh token url

--- src/o2spotify.cpp
+++ src/o2spotify.cpp
@@ -6,4 +6,5 @@ static const char *SpotifyTokenUrl = "https://accounts.spotify.com/api/token";
 O2Spotify::O2Spotify(QObject *parent): O2(parent) {
     setRequestUrl(SpotifyEndpoint);
     setTokenUrl(SpotifyTokenUrl);
+    setRefreshTokenUrl(SpotifyTokenUrl);
 }
