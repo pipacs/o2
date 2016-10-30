9a1cb0ab444b3527dee30f8ce2aa9d00d381a2c3
Lukas Vogel <lukedirtwalker@gmail.com>
2016-10-30 18:19:22 +0100

    Add all spotify scopes to O2Spotify

--- src/o2spotify.cpp
+++ src/o2spotify.cpp
@@ -8,3 +8,35 @@ O2Spotify::O2Spotify(QObject *parent): O2(parent) {
     setTokenUrl(SpotifyTokenUrl);
     setRefreshTokenUrl(SpotifyTokenUrl);
 }
+
+const QString O2Spotify::Scope::PLAYLIST_READ_PRIVATE = "playlist-read-private";
+const QString O2Spotify::Scope::PLAYLIST_READ_COLLABORATIVE = "playlist-read-collaborative";
+const QString O2Spotify::Scope::PLAYLIST_MODIFY_PUBLIC = "playlist-modify-public";
+const QString O2Spotify::Scope::PLAYLIST_MODIFY_PRIVATE = "playlist-modify-private";
+const QString O2Spotify::Scope::STREAMING = "streaming";
+const QString O2Spotify::Scope::USER_FOLLOW_MODIFY = "user-follow-modify";
+const QString O2Spotify::Scope::USER_FOLLOW_READ = "user-follow-read";
+const QString O2Spotify::Scope::USER_LIBRARY_READ = "user-library-read";
+const QString O2Spotify::Scope::USER_LIBRARY_MODIFY = "user-library-modify";
+const QString O2Spotify::Scope::USER_READ_PRIVATE = "user-read-private";
+const QString O2Spotify::Scope::USER_READ_BIRTHDATE = "user-read-birthdate";
+const QString O2Spotify::Scope::USER_READ_EMAIL = "user-read-email";
+const QString O2Spotify::Scope::USER_TOP_READ = "user-top-read";
+
+QStringList O2Spotify::Scope::allScopesList() {
+    QStringList result;
+    return result
+            << PLAYLIST_READ_PRIVATE
+            << PLAYLIST_READ_COLLABORATIVE
+            << PLAYLIST_MODIFY_PUBLIC
+            << PLAYLIST_MODIFY_PRIVATE
+            << STREAMING
+            << USER_FOLLOW_MODIFY
+            << USER_FOLLOW_READ
+            << USER_LIBRARY_READ
+            << USER_LIBRARY_MODIFY
+            << USER_READ_PRIVATE
+            << USER_READ_BIRTHDATE
+            << USER_READ_EMAIL
+            << USER_TOP_READ;
+}
