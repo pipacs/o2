1f9604edbddfadc323ec7801e625a846e262fc59
Nikolay Tischenko <n.tischenko@movavi.com>
2017-02-10 11:28:31 +0700

    Fix warnings about symbols import on windows in case of static library build

--- src/o0export.h
+++ src/o0export.h
@@ -2,7 +2,7 @@
 
 #ifndef O0_EXPORT
 // For exporting symbols from Windows' DLLs
-#ifdef _WIN32
+#if defined (_WIN32) && defined(O2_SHARED_LIB)
     #ifdef O2_DLL_EXPORT
         #define O0_EXPORT __declspec(dllexport)
     #else
