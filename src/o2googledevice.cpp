03d5097b82e51419f27703bf2f5576018eb60aaf
Patrik Dahlström <risca@powerlamerz.org>
2020-11-15 02:29:25 +0100

    Update URLs used for O2GoogleDevice
    Since O2GoogleDevice now no longer have anything in common with
    O2Google, change parent class to O2.

--- src/o2googledevice.cpp
+++ src/o2googledevice.cpp
@@ -1,13 +1,15 @@
 #include "o2googledevice.h"
 
+static const char *GoogleDeviceTokenUrl = "https://oauth2.googleapis.com/token";
+static const char *GoogleDeviceRefreshUrl = "https://oauth2.googleapis.com/token";
 static const char *GoogleDeviceEndpoint = "https://oauth2.googleapis.com/device/code";
 // Google uses a different grant type value than specified in RFC 8628
 static const char *GoogleDeviceGrantType = "http://oauth.net/grant_type/device/1.0";
 
-O2GoogleDevice::O2GoogleDevice(QObject *parent) : O2Google(parent) {
+O2GoogleDevice::O2GoogleDevice(QObject *parent) : O2(parent) {
     setGrantFlow(GrantFlowDevice);
     setGrantType(GoogleDeviceGrantType);
-    // O2Google() already set the correct token and refresh token URLs.
-    // However, the request endpoint is different.
     setRequestUrl(GoogleDeviceEndpoint);
+    setTokenUrl(GoogleDeviceTokenUrl);
+    setRefreshTokenUrl(GoogleDeviceRefreshUrl);
 }
