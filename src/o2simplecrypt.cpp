bdedb0e714efd392d6c391084baa98e8488c0c63
Victor Jungbluth <victor.jungbluth@corsair.com>
2021-11-15 13:56:00 +0100

    - add missing header include

--- src/o2simplecrypt.cpp
+++ src/o2simplecrypt.cpp
@@ -31,6 +31,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <QDateTime>
 #include <QCryptographicHash>
 #include <QDataStream>
+#include <QIODevice>
 
 O0SimpleCrypt::O0SimpleCrypt():
     m_key(0),
