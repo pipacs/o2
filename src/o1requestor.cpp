e7e8163450c1e3ce4841a63fbd03884fefccb84a
Victor Jungbluth <victor.jungbluth@corsair.com>
2021-11-15 14:43:12 +0100

    - use currentSecsSinceEpoch instead of obsoleted toTime_t
    NOTE: toTime_t return 32bit platform default unsigned int, currentSecsSinceEpoch returns qint64 (long long). However, Qt ensures that qint64 is working on all Qt supported platforms.

--- src/o1requestor.cpp
+++ src/o1requestor.cpp
@@ -45,7 +45,11 @@ QNetworkRequest O1Requestor::setup(const QNetworkRequest &req, const QList<O0Req
     oauthParams.append(O0RequestParameter(O2_OAUTH_TOKEN, authenticator_->token().toLatin1()));
     oauthParams.append(O0RequestParameter(O2_OAUTH_SIGNATURE_METHOD, authenticator_->signatureMethod().toLatin1()));
     oauthParams.append(O0RequestParameter(O2_OAUTH_NONCE, O1::nonce()));
+#if QT_VERSION >= 0x050800
+    oauthParams.append(O0RequestParameter(O2_OAUTH_TIMESTAMP, QString::number(QDateTime::currentSecsSinceEpoch()).toLatin1()));
+#else
     oauthParams.append(O0RequestParameter(O2_OAUTH_TIMESTAMP, QString::number(QDateTime::currentDateTimeUtc().toTime_t()).toLatin1()));
+#endif
 
     // Add signature parameter
     oauthParams.append(O0RequestParameter(O2_OAUTH_SIGNATURE, authenticator_->generateSignature(oauthParams, req, signingParameters, operation)));
